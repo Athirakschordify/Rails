787e05736bebbff393bd53a848f5ec4f595e2d3f
athira <athira.ks@chordify.com>
2023-03-13 23:31:48 +0530

    store

---
+++ store/config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  resources :items
+  resources :roles
+  #resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index
+  devise_for :users
+  scope '/admin' do
+    resources :users
+  end
+  root to: 'items#index'
+  
+  
+
+end
